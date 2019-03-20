1e06328b15b08cccc0a284f95537ff6e671b8f5f
slydor <aprange@gmx.net>
2019-03-20 21:33:09 +0100

    added parser creator

--- parser/parse.ts
+++ parser/parse.ts
@@ -1,47 +1,4 @@
-import * as nearley from 'nearley';
 import * as grammar from '../grammar/rule';
+import { createParse } from './createParse';
 
-export const parse = (
-    text: string,
-    debug: boolean = false,
-    print: boolean = false
-) => {
-    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-    try {
-        parser.feed(text);
-        const { results } = parser;
-        if (results.length === 1) {
-            if (print) {
-                console.log(
-                    'parsed result:\n',
-                    JSON.stringify(results[0], undefined, 2)
-                );
-            }
-            return results[0];
-        }
-
-        if (results.length > 1) {
-            if (debug) {
-                console.log(`grammar is ambiguous for text [${text}]`);
-                console.log(JSON.stringify(results, undefined, 2));
-            }
-            if (print) {
-                console.log(
-                    'parsed result:\n',
-                    JSON.stringify(results[0], undefined, 2)
-                );
-            }
-            return results[0];
-        }
-
-        if (debug && results.length === 0) {
-            console.log(`no parsing result for text [${text}]`);
-        }
-    } catch (error) {
-        if (debug) {
-            console.log(`parsing error for text [${text}]`);
-            console.log(error);
-        }
-    }
-    return undefined;
-};
+export const parse = createParse(grammar);
